a9d35120f7e0c857da182337b2a1c0955ab21ddd
Pavel-Freeman <56322883+Pavel-Freeman@users.noreply.github.com>
2020-06-29 12:49:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = "utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href = "styles/header.css">
+    <link rel = "stylesheet" href = "styles/content.css">
+    <link rel = "stylesheet" href = "styles/footer.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-360.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-411.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-414.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-375.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-768.css">
+    <link rel = "stylesheet" href = "styles/adaptive/adaptive-1024.css">
+    <title>LevelUp</title>
+</head>
+<body>
+
+    <div class = "header">
+        <a href = "#" class = "logo__link">Logo</a>
+        <a href = "#" class = "head__link">About</a>
+        <a href = "#" class = "head__link">Values</a>
+        <a href = "#" class = "head__link">News</a>
+        <a href = "#" class = "head__link">Contact</a>
+        <a href = "#" class = "head__link">Clients</a>
+        <a href = "#" class = "head__link">Partners</a>
+        <div class = "burger">
+            <input type = "checkbox" id = "check-menu">
+            <label for = "check-menu" class = "menu-burger"></label>
+            <div class="burger-line first"></div>
+            <div class="burger-line second"></div>
+            <div class="burger-line third"></div>
+            <div class="burger-line fourth"></div>
+            <div class = "links__burger">
+                <a href = "#" class = "link__burger">Link</a>
+                <a href = "#" class = "link__burger">Link</a>
+                <a href = "#" class = "link__burger">Link</a>
+                <a href = "#" class = "link__burger">Link</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class = "sidebar">
+        <h3 class = "menu__sidebar">Menu</h3>
+        <a href = "#" class = "link__sidebar">Link</a>
+        <a href = "#" class = "link__sidebar">Link</a>
+        <a href = "#" class = "link__sidebar">Link</a>
+        <a href = "#" class = "link__sidebar">Link</a>
+    </div>
+
+    <div class = "articles">
+        <div class = "article">
+            <p class = "heading-text-content">Heading</p>
+            <p class = "text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+        <div class = "article">
+            <p class = "heading-text-content">Heading</p>
+            <p class = "text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+        <div class = "article">
+            <p class = "heading-text-content">Heading</p>
+            <p class = "text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            </div>
+    </div>
+
+
+    <div class = "adipisicings"> 
+            <div class = "adipisicing">
+                <p class = "advertising__object1">AD</p>
+                <p class = "advertising__object2">AD</p>
+                </div>
+        <div class = "adipisicing">
+            <p class = "advertising__object1">AD</p>
+            <p class = "advertising__object2">AD</p>
+            </div>
+        <div class = "adipisicing">
+            <p class = "advertising__object1">AD</p>
+            <p class = "advertising__object2">AD</p>
+            </div>
+    </div>
+
+    <div class = "pages" >
+        <a href = "#" class = "page__link1">1</a>
+        <a href = "#" class = "page__link">2</a>
+        <a href = "#" class = "page__link">3</a>
+        <a href = "#" class = "page__link">4</a>
+        <a href = "#" class = "page__link">5</a>
+        <a href = "#" class = "page__link">>></a>
+    </div>
+
+
+    <div class = "foot"><h4>Footer</h4></div>
+    <div class = "powered"><p>Powered by <a href = "href="https://www.w3schools.com/w3css/default.asp">w3.css</a></p></div>
+
+
+</body>
